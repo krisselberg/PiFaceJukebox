0402595d9b3b154fb52a4159756175f5ff3b91ec
Kristoffer Selberg <kselberg@dynamic-oit-ip4-wifirestricted01-10-16-182-105.princeton.edu>
2023-09-22 20:01:47 -0400

    play music in the background

--- main.py
+++ main.py
@@ -1,8 +1,16 @@
-from pydub import AudioSegment
-from pydub.playback import play
+import pygame
 import cv2
 import pickle
 import os
+from pydub import AudioSegment
+
+def convert_to_mp3(m4a_file):
+    mp3_file = m4a_file.replace('.m4a', '.mp3')
+    AudioSegment.from_file(m4a_file).export(mp3_file, format='mp3')
+    return mp3_file
+
+# Initialize pygame mixer
+pygame.mixer.init()
 
 # Load the model and label dictionary (if saved previously)
 model = cv2.face.LBPHFaceRecognizer_create()
@@ -13,23 +21,31 @@ with open('label_dict.pkl', 'rb') as f:
 # Start the video capture
 cap = cv2.VideoCapture(0)
 
+is_playing = False  # Variable to track if a song is currently playing
+person_name = ""
+
 while True:
     ret, frame = cap.read()
     grayscale_img = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
     label, confidence = model.predict(grayscale_img)
     print(label_dict[label], confidence)
-    if confidence < 100:  # You can adjust this value based on your requirements
+    if confidence < 50 and not is_playing:
         person_name = label_dict[label]
-        mp3_file = f"music/{person_name}.m4a"
+        m4a_file = f"music/{person_name}.m4a"
             
-        if os.path.exists(mp3_file):
-            song = AudioSegment.from_file(mp3_file, format="m4a")
-            play(song)
+        if os.path.exists(m4a_file):
+            mp3_file = convert_to_mp3(m4a_file)
+            pygame.mixer.music.load(mp3_file)
+            pygame.mixer.music.play()
+            is_playing = True
     
-        cv2.putText(frame, person_name, (50, 50), cv2.FONT_HERSHEY_COMPLEX, 1, (0, 255, 0), 2)
-        cv2.imshow('Frame', frame)
-        if cv2.waitKey(1) & 0xFF == ord('q'):
-            break
+    elif pygame.mixer.music.get_busy() == 0:
+        is_playing = False  # Set the flag to False if no song is playing
+    
+    cv2.putText(frame, person_name, (50, 50), cv2.FONT_HERSHEY_COMPLEX, 1, (0, 255, 0), 2)
+    cv2.imshow('Frame', frame)
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
 
 cap.release()
 cv2.destroyAllWindows()
